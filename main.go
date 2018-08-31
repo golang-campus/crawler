621c4f52c46b6f50f5dad344910925d647d97f38
LucaHome <yangzoucn@163.com>
2018-08-31 15:59:10 +0800

    finish Get URL & GBK 2 UTF8

--- main.go
+++ main.go
@@ -2,28 +2,46 @@ package main
 
 import (
 	"./config"
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 
-	//
-	resp, e := http.Get(config.BaseUrl)
-	if e != nil {
-		panic(e)
+	resp, err := http.Get(config.BaseUrl)
+	if err != nil {
+		panic(err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		fmt.Println("Error: status code is ", resp.StatusCode)
 		return
-	} else {
-		all, e := ioutil.ReadAll(resp.Body)
-		if e != nil {
-			panic(e)
-		}
-		fmt.Printf("%s\n", all)
 	}
+
+	//将网页文本的GBK转UTF8
+	e := determineEncoding(resp.Body)
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	all, err := ioutil.ReadAll(utf8Reader)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", all)
+
+}
+
+//检测网页编码
+func determineEncoding(r io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
